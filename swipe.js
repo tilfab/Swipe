896ba38666c5cc856abcad3888c8bf20dc595127
lars <lars@tilf.se>
2015-04-29 10:18:03 +0200

    added direction as third argument to options.callback function

--- swipe.js
+++ swipe.js
@@ -147,7 +147,7 @@ function Swipe(container, options) {
     }
 
     index = to;
-    offloadFn(options.callback && options.callback(index, slides[index]));
+    offloadFn(options.callback && options.callback(index, slides[index], direction));
   }
 
   function move(index, dist, speed) {
@@ -395,7 +395,8 @@ function Swipe(container, options) {
 
           }
 
-          options.callback && options.callback(index, slides[index]);
+          var callbackDirection = (direction) ? 1 : -1;
+          options.callback && options.callback(index, slides[index], callbackDirection);
 
         } else {
 
